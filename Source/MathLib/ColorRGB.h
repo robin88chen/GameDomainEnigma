d584f773722550901715cb39b45173c9ff77cb74
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-04-24 17:22:12 +0800

    [Math] add Colors

---
+++ Source/MathLib/ColorRGB.h
@@ -0,0 +1,106 @@
+﻿/********************************************************************
+ * \file   ColorRGB.h
+ * \brief  
+ * 
+ * \author Lancelot 'Robin' Chen
+ * \date   April 2022
+ *********************************************************************/
+#ifndef _EN_COLOR_RGB_H
+#define _EN_COLOR_RGB_H
+
+/** @file EnColorRGB.h */
+namespace Enigma::MathLib
+{
+    /** Color RGB Class
+    @remarks
+    The components are intended to be in the interval [0,1].
+     For accessing of colors by array index, the map is 0 = red, 1 = green,
+     and 2 = blue.
+    */
+    class ColorRGB
+    {
+    public:
+        // Construction.  The components are intended to be in the interval [0,1].
+        // For accessing of colors by array index, the map is 0 = red, 1 = green,
+        // and 2 = blue.
+        ColorRGB(); ///< initial values (0,0,0)
+        ColorRGB(float r, float g, float b);
+        /// index -- 0: red, 1: green, 2: blue
+        ColorRGB(float tuple[3]);
+        ColorRGB(const ColorRGB& c) = default;
+        ColorRGB(ColorRGB&&) = default;
+        /// rgb = 0x00rrggbb
+        ColorRGB(unsigned int rgb);
+
+        /** @name member access */
+        //@{
+        operator const float*() const;
+        operator float*();
+        float operator[](int i) const;
+        float& operator[](int i);
+        float R() const;
+        float& R();
+        float G() const;
+        float& G();
+        float B() const;
+        float& B();
+        unsigned int GetRGB() const;
+        //@}
+
+        /// assignment
+        ColorRGB& operator=(const ColorRGB&) = default;
+        ColorRGB& operator=(ColorRGB&&) = default;
+
+        /** @name comparison */
+        //@{
+        bool operator==(const ColorRGB& c) const;
+        bool operator!=(const ColorRGB& c) const;
+        bool operator<(const ColorRGB& c) const;
+        bool operator<=(const ColorRGB& c) const;
+        bool operator>(const ColorRGB& c) const;
+        bool operator>=(const ColorRGB& c) const;
+        //@}
+
+        /** @name arithmetic operations */
+        //@{
+        ColorRGB operator+(const ColorRGB& c) const;
+        ColorRGB operator-(const ColorRGB& c) const;
+        ColorRGB operator*(const ColorRGB& c) const;
+        ColorRGB operator*(float scalar) const;
+        //@}
+
+        /** @name arithmetic updates */
+        //@{
+        ColorRGB& operator+=(const ColorRGB& c);
+        ColorRGB& operator-=(const ColorRGB& c);
+        ColorRGB& operator*=(const ColorRGB& c);
+        ColorRGB& operator*=(float scalar);
+        //@}
+
+        // Transform the color channels to [0,1].  Clamp sets negative values to
+        // zero and values larger than one to one.  ScaleByMax assumes the color
+        // channels are nonnegative, finds the maximum color channel, and divides
+        // all channels by that value.
+        /** Transform the color channels to [0,1].  
+        Clamp sets negative values to zero and values larger than one to one. */
+        ColorRGB Clamp() const;
+        /** Transform the color channels to [0,1].
+        ScaleByMax assumes the color channels are nonnegative, finds the maximum color channel, and divides
+         all channels by that value. */
+        ColorRGB ScaleByMax() const;
+
+        static const ColorRGB BLACK; ///< = (0,0,0) 
+        static const ColorRGB WHITE; ///< = (1,1,1)
+        static const ColorRGB INVALID; ///< = (-1,-1,-1)
+        static const ColorRGB ZERO; ///< = (0,0,0)
+
+    private:
+        float m_tuple[3];
+        unsigned int m_rgbValue;
+    };
+
+    /** scalar * color */
+    ColorRGB operator*(float scalar, const ColorRGB& c);
+};
+
+#endif
--- Source/MathLib/Vector3.h
+++ Source/MathLib/Vector3.h
@@ -10,103 +10,100 @@
 
 #include "Vector4.h"
 
-namespace Enigma
+namespace Enigma::MathLib
 {
-    namespace MathLib
+    /** Math Lib Vector3 */
+    class Vector3
     {
-        /** Math Lib Vector3 */
-        class Vector3
+    public:
+        // construction
+        Vector3();  // uninitialized
+        Vector3(float x, float y, float z);
+        Vector3(float* f);
+
+        /** @name coordinate access */
+        //@{
+        operator const float* () const;
+        operator float* ();
+        float operator[] (int i) const;
+        float& operator[] (int i);
+        float X() const;
+        float& X();
+        float Y() const;
+        float& Y();
+        float Z() const;
+        float& Z();
+        //@}
+
+        /** @name assignment */
+        //@{
+        Vector3& operator= (const Vector4& v);
+        //@}
+
+        /** @name comparison */
+        //@{
+        bool operator== (const Vector3& v) const;  ///< 浮點數值比較
+        bool operator!= (const Vector3& v) const;   ///< 浮點數值比較
+        bool operator<  (const Vector3& v) const;   ///< 比較陣列記憶體內容
+        bool operator<= (const Vector3& v) const;    ///< 比較陣列記憶體內容
+        bool operator>  (const Vector3& v) const;   ///< 比較陣列記憶體內容
+        bool operator>= (const Vector3& v) const;   ///< 比較陣列記憶體內容
+        //@}
+
+        /** @name arithmetic operations */
+        //@{
+        Vector3 operator+ (const Vector3& v) const;
+        Vector3 operator- (const Vector3& v) const;
+        Vector3 operator* (float scalar) const;
+        Vector3 operator/ (float scalar) const;
+        Vector3 operator- () const;
+        //@}
+
+        /** @name arithmetic updates */
+        //@{
+        Vector3& operator+= (const Vector3& v);
+        Vector3& operator-= (const Vector3& v);
+        Vector3& operator*= (float scalar);
+        Vector3& operator/= (float scalar);
+        //@}
+
+        /** @name vector operations */
+        //@{
+        float Length() const;
+        float SquaredLength() const;
+        float Dot(const Vector3& v) const;
+        Vector3 Normalize() const;
+        /// The cross products are computed using the left-handed rule.
+        Vector3 Cross(const Vector3& v) const;
+        /// The cross products are computed using the left-handed rule.
+        Vector3 UnitCross(const Vector3& v) const;
+        //@}
+
+        /** @name special vectors */
+        //@{
+        static const Vector3 ZERO;
+        static const Vector3 UNIT_X;
+        static const Vector3 UNIT_Y;
+        static const Vector3 UNIT_Z;
+        //@}
+
+    private:
+        // support for none-equal comparisons
+        int CompareArrays(const Vector3& v) const;
+        /** member data */
+        union
         {
-        public:
-            // construction
-            Vector3();  // uninitialized
-            Vector3(float x, float y, float z);
-            Vector3(float* f);
-
-            /** @name coordinate access */
-            //@{
-            operator const float* () const;
-            operator float* ();
-            float operator[] (int i) const;
-            float& operator[] (int i);
-            float X() const;
-            float& X();
-            float Y() const;
-            float& Y();
-            float Z() const;
-            float& Z();
-            //@}
-
-            /** @name assignment */
-            //@{
-            Vector3& operator= (const Vector4& v);
-            //@}
-
-            /** @name comparison */
-            //@{
-            bool operator== (const Vector3& v) const;  ///< 浮點數值比較
-            bool operator!= (const Vector3& v) const;   ///< 浮點數值比較
-            bool operator<  (const Vector3& v) const;   ///< 比較陣列記憶體內容
-            bool operator<= (const Vector3& v) const;    ///< 比較陣列記憶體內容
-            bool operator>  (const Vector3& v) const;   ///< 比較陣列記憶體內容
-            bool operator>= (const Vector3& v) const;   ///< 比較陣列記憶體內容
-            //@}
-
-            /** @name arithmetic operations */
-            //@{
-            Vector3 operator+ (const Vector3& v) const;
-            Vector3 operator- (const Vector3& v) const;
-            Vector3 operator* (float scalar) const;
-            Vector3 operator/ (float scalar) const;
-            Vector3 operator- () const;
-            //@}
-
-            /** @name arithmetic updates */
-            //@{
-            Vector3& operator+= (const Vector3& v);
-            Vector3& operator-= (const Vector3& v);
-            Vector3& operator*= (float scalar);
-            Vector3& operator/= (float scalar);
-            //@}
-
-            /** @name vector operations */
-            //@{
-            float Length() const;
-            float SquaredLength() const;
-            float Dot(const Vector3& v) const;
-            Vector3 Normalize() const;
-            /// The cross products are computed using the left-handed rule.
-            Vector3 Cross(const Vector3& v) const;
-            /// The cross products are computed using the left-handed rule.
-            Vector3 UnitCross(const Vector3& v) const;
-            //@}
-
-            /** @name special vectors */
-            //@{
-            static const Vector3 ZERO;
-            static const Vector3 UNIT_X;
-            static const Vector3 UNIT_Y;
-            static const Vector3 UNIT_Z;
-            //@}
-
-        private:
-            // support for none-equal comparisons
-            int CompareArrays(const Vector3& v) const;
-            /** member data */
-            union
+            struct
             {
-                struct
-                {
-                    float m_x;
-                    float m_y;
-                    float m_z;
-                };
-                float m_tuple[3];
+                float m_x;
+                float m_y;
+                float m_z;
             };
+            float m_tuple[3];
         };
-        /** scalar * vector3 */
-        Vector3 operator* (float scalar, const Vector3& v);
-    }
+    };
+    /** scalar * vector3 */
+    Vector3 operator* (float scalar, const Vector3& v);
 }
 
 #endif // !_MATH_VECTOR3_H

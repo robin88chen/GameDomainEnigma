971bad51fd754c71ae3ade5aceba02417ad01733
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-06-12 21:24:08 +0800

    [Graphic Kernel] add projects

---
+++ Source/GraphicKernel/GraphicErrors.cpp
@@ -0,0 +1,26 @@
+﻿#include "GraphicErrors.h"
+
+using namespace Enigma::Graphics;
+
+const ErrorCategory ErrorCategory::ms_category;
+
+std::string ErrorCategory::message(int err) const
+{
+    switch (static_cast<ErrorCode>(err))
+    {
+    case ErrorCode::ok: return "OK";
+    }
+    return "Unknown";
+}
+
+const char* ErrorCategory::name() const noexcept
+{
+    return "Graphics Error Category";
+}
+namespace Enigma::Graphics
+{
+    std::error_code make_error_code(ErrorCode ec)
+    {
+        return std::error_code(static_cast<int>(ec), ErrorCategory::get());
+    }
+}
---
+++ Source/GraphicKernel/IGraphicAPI.h
@@ -0,0 +1,32 @@
+﻿/********************************************************************
+ * \file   IGraphicAPI.h
+ * \brief  
+ * 
+ * \author Lancelot 'Robin' Chen
+ * \date   June 2022
+ *********************************************************************/
+#ifndef GRAPHIC_API_INTERFACE_H
+#define GRAPHIC_API_INTERFACE_H
+
+namespace Enigma::Graphics
+{
+    class IGraphicAPI
+    {
+    public:
+        enum class APIVersion
+        {
+            API_Dx9,
+            API_Dx11,
+            API_EGL,
+        };
+    public:
+        IGraphicAPI();
+        IGraphicAPI(const IGraphicAPI&) = delete;
+        virtual ~IGraphicAPI();
+        IGraphicAPI& operator=(const IGraphicAPI&) = delete;
+
+        static IGraphicAPI* Instance();
+    };
+}
+
+#endif // GRAPHIC_API_INTERFACE_H

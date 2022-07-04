4a7640ea48b17a0a9a79171100aedadc2083f9cc
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-07-04 15:20:09 +0800

    [Graphics] Shader Program, Vertex Declaration interface

---
+++ Source/Frameworks/StringFormat.h
@@ -0,0 +1,25 @@
+﻿/*********************************************************************
+ * \file   StringFormat.h
+ * \brief  
+ * 
+ * \author Lancelot 'Robin' Chen
+ * \date   July 2022
+ *********************************************************************/
+#ifndef EN_UTIL_STRING_FORMAT_H
+#define EN_UTIL_STRING_FORMAT_H
+
+#include <memory>
+#include <string>
+#include <stdexcept>
+
+template<typename ... Args>
+std::string string_format(const std::string& format, Args ... args)
+{
+    size_t size = snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
+    if (size <= 0) { throw std::runtime_error("Error during formatting."); }
+    std::unique_ptr<char[]> buf(new char[size]);
+    snprintf(buf.get(), size, format.c_str(), args ...);
+    return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
+}
+
+#endif // EN_UTIL_STRING_FORMAT_H
---
+++ Source/GraphicKernel/IShaderProgram.cpp
@@ -0,0 +1,18 @@
+﻿#include "IShaderProgram.h"
+
+using namespace Enigma::Graphics;
+
+IShaderProgram::IShaderProgram(const std::string& name, const IVertexShaderPtr& vtx_shader,
+    const IPixelShaderPtr& pix_shader)
+{
+    m_name = name;
+    m_vtxShader = vtx_shader;
+    m_pixShader = pix_shader;
+}
+
+IShaderProgram::~IShaderProgram()
+{
+    m_vtxShader = nullptr;
+    m_pixShader = nullptr;
+}
+

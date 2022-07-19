0f759101fa2215341fcee0170bc43b1f3c06b902
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-07-19 10:59:38 +0800

    [WIP] Texture Egl

---
+++ Source/GraphicAPIEgl/TextureEgl.cpp
@@ -0,0 +1,16 @@
+﻿#include "TextureEgl.h"
+#include "Platforms/MemoryMacro.h"
+#include "Platforms/PlatformLayer.h"
+#include "GraphicKernel/GraphicErrors.h"
+#include "GraphicKernel/GraphicEvents.h"
+#include "Frameworks/EventPublisher.h"
+#include "MathLib/Rect.h"
+#include "FileSystem/IFile.h"
+#include <cassert>
+
+using namespace Enigma::Devices;
+using ErrorCode = Enigma::Graphics::ErrorCode;
+#if TARGET_PLATFORM == PLATFORM_ANDROID
+#include "png.h"
+
+#endif

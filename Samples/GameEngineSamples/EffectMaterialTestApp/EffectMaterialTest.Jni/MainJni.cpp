e5a26117df525fafca37ff2e0477be23a2c700cb
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-09-08 10:56:04 +0800

    [EffectMaterial Test] android app

---
+++ Samples/GameEngineSamples/EffectMaterialTestApp/EffectMaterialTest.Jni/MainJni.cpp
@@ -0,0 +1,31 @@
+﻿/**********************************
+ Java Native Interface library
+**********************************/
+#include <jni.h>
+#include "EffectMaterialTest.h"
+#include "FileSystem/FileSystem.h"
+#include "FileSystem/AndroidMountPath.h"
+
+EffectMaterialTest* g_app = new EffectMaterialTest("Effect Material Test App");
+
+/** This is the C++ implementation of the Java native method.
+@param env Pointer to JVM environment
+@param thiz Reference to Java this object
+*/
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_EffectMaterialTestApp_EffectMaterialTestApp_initBridgeCallback(JNIEnv * env, jobject thiz)
+{
+    g_app->InitBridgeCallback();
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_EffectMaterialTestApp_EffectMaterialTestApp_initAdditionalMountPath(JNIEnv * env, jobject thiz)
+{
+    if (Enigma::FileSystem::FileSystem::Instance())
+    {
+        Enigma::FileSystem::FileSystem::Instance()->AddMountPath(std::make_shared<Enigma::FileSystem::AndroidMountPath>("",
+            EffectMaterialTest::MediaPathName));
+    }
+}

0d2b9fc5c42000a6041ad86cca4dea0fae7ba0cd
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-09-13 08:42:49 +0800

    [Engine] effect material manager test app

---
+++ Samples/GameEngineSamples/EffectMaterialManagerTestApp/EffectMaterialManagerTest.Jni/MainJni.cpp
@@ -0,0 +1,31 @@
+﻿/**********************************
+ Java Native Interface library
+**********************************/
+#include <jni.h>
+#include "EffectMaterialManagerTest.h"
+#include "FileSystem/FileSystem.h"
+#include "FileSystem/AndroidMountPath.h"
+
+EffectMaterialManagerTest* g_app = new EffectMaterialManagerTest("Effect Material Manager Test App");
+
+/** This is the C++ implementation of the Java native method.
+@param env Pointer to JVM environment
+@param thiz Reference to Java this object
+*/
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_EffectMaterialManagerTestApp_EffectMaterialManagerTestApp_initBridgeCallback(JNIEnv * env, jobject thiz)
+{
+    g_app->InitBridgeCallback();
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_EffectMaterialManagerTestApp_EffectMaterialManagerTestApp_initAdditionalMountPath(JNIEnv * env, jobject thiz)
+{
+    if (Enigma::FileSystem::FileSystem::Instance())
+    {
+        Enigma::FileSystem::FileSystem::Instance()->AddMountPath(std::make_shared<Enigma::FileSystem::AndroidMountPath>("",
+            EffectMaterialManagerTest::MediaPathName));
+    }
+}
---
+++ Samples/GameEngineSamples/EffectMaterialManagerTestApp/EffectMaterialManagerTestApp/app/src/main/java/com/EffectMaterialManagerTestApp/EffectMaterialManagerTestApp.java
@@ -0,0 +1,25 @@
+
+package com.EffectMaterialManagerTestApp;
+import android.os.Bundle;
+import lib.bridge.enigma.ApplicationBridgeActivity;
+
+public class EffectMaterialManagerTestApp extends ApplicationBridgeActivity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        initBridgeCallback();
+        super.onCreate(savedInstanceState);
+        initAdditionalMountPath();
+    }
+    public native void initBridgeCallback();
+    public native void initAdditionalMountPath();
+    /* This is the static constructor used to load the
+     * 'ParticlePrimitiveTest' library when the class is
+     * loaded.
+     */
+    static {
+        System.loadLibrary("EffectMaterialManagerTest_Jni");
+    }
+}

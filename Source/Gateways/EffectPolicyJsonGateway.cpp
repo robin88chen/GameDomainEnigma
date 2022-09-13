ba699a639e38ccfdde6e23417f3c9bea357d21ad
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-09-13 09:18:03 +0800

    [Architecture] add gateway projects

---
+++ Source/Gateways/EffectPolicyJsonGateway.cpp
@@ -0,0 +1,2 @@
+﻿#include "EffectPolicyJsonGateway.h"
+
---
+++ Source/Gateways/EffectPolicyJsonGateway.h
@@ -0,0 +1,16 @@
+﻿/*********************************************************************
+ * \file   EffectPolicyJsonGateway.h
+ * \brief  
+ * 
+ * \author Lancelot 'Robin' Chen
+ * \date   September 2022
+ *********************************************************************/
+#ifndef EFFECT_POLICY_JSON_GATEWAY_H
+#define EFFECT_POLICY_JSON_GATEWAY_H
+
+namespace Enigma::Gateways
+{
+    
+}
+
+#endif // EFFECT_POLICY_JSON_GATEWAY_H

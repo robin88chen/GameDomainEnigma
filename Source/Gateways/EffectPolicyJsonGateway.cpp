63d922e578ba05cc4649d5bc10dbc0161063b609
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-09-14 18:08:54 +0800

    [Gateway][WIP] vertex, pixel shader meta

--- Source/Gateways/EffectPolicyJsonGateway.cpp
+++ Source/Gateways/EffectPolicyJsonGateway.cpp
@@ -7,10 +7,16 @@ using namespace Enigma::Gateways;
 using namespace Enigma::Engine;
 
 #define VERTEX_SHADER_LIST_TOKEN "VertexShaderList"
+#define PIXEL_SHADER_LIST_TOKEN "PixelShaderList"
+#define NAME_TOKEN	"Name"
+#define FILE_TOKEN	"File"
+#define PROFILE_TOKEN	"Profile"
+#define ENTRY_TOKEN	"Entry"
+#define VTX_DECL_TOKEN "VtxDecl"
+#define API_TOKEN	"API"
 
 EffectPolicyJsonGateway::EffectPolicyJsonGateway()
 {
-    
 }
 
 std::optional<EffectCompilingPolicy> EffectPolicyJsonGateway::Deserialize(const std::string& json)
@@ -24,12 +30,71 @@ std::optional<EffectCompilingPolicy> EffectPolicyJsonGateway::Deserialize(const 
         return std::nullopt;
     }
     m_vertexShaderGateways = DeserializeVertexShaderList(json_doc[VERTEX_SHADER_LIST_TOKEN]);
+    m_pixelShaderGateways = DeserializePixelShaderList(json_doc[PIXEL_SHADER_LIST_TOKEN]);
 
     return std::nullopt;
 }
 
 std::vector<EffectPolicyJsonGateway::VertexShaderGatewayMeta> EffectPolicyJsonGateway::DeserializeVertexShaderList(
-    const rapidjson::Value& shader_list)
+    const rapidjson::Value& shader_list) const
 {
-    return std::vector<VertexShaderGatewayMeta>{};
+    auto shader_metas = std::vector<VertexShaderGatewayMeta>{};
+    if (FATAL_LOG_EXPR(!shader_list.IsArray()))
+    {
+        std::string ss = string_format("vertex shader list is not a array");
+        LOG(Info, ss);
+        return shader_metas;
+    }
+    for (auto shader_it = shader_list.GetArray().Begin(); shader_it != shader_list.GetArray().End(); ++shader_it)
+    {
+        shader_metas.emplace_back(DeserializeVertexShaderMeta(*shader_it));
+    }
+
+    return shader_metas;
+}
+
+std::vector<EffectPolicyJsonGateway::PixelShaderGatewayMeta> EffectPolicyJsonGateway::DeserializePixelShaderList(
+    const rapidjson::Value& shader_list) const
+{
+    auto shader_metas = std::vector<PixelShaderGatewayMeta>{};
+    if (FATAL_LOG_EXPR(!shader_list.IsArray()))
+    {
+        std::string ss = string_format("pixel shader list is not a array");
+        LOG(Info, ss);
+        return shader_metas;
+    }
+    for (auto shader_it = shader_list.GetArray().Begin(); shader_it != shader_list.GetArray().End(); ++shader_it)
+    {
+        shader_metas.emplace_back(DeserializePixelShaderMeta(*shader_it));
+    }
+
+    return shader_metas;
+}
+
+EffectPolicyJsonGateway::VertexShaderGatewayMeta EffectPolicyJsonGateway::DeserializeVertexShaderMeta(const rapidjson::Value& shader) const
+{
+    assert(!shader.IsNull());
+
+    VertexShaderGatewayMeta meta;
+    meta.m_shaderName = shader[NAME_TOKEN].GetString();
+    meta.m_api = m_apiReference.at(shader[API_TOKEN].GetString());
+    meta.m_filename = shader[FILE_TOKEN].GetString();
+    meta.m_profile = shader[PROFILE_TOKEN].GetString();
+    meta.m_entry = shader[ENTRY_TOKEN].GetString();
+    meta.m_formatCode = shader[VTX_DECL_TOKEN].GetString();
+    meta.m_layoutName = meta.m_shaderName + ":" + meta.m_formatCode;
+    return meta;
+}
+
+EffectPolicyJsonGateway::PixelShaderGatewayMeta EffectPolicyJsonGateway::DeserializePixelShaderMeta(const rapidjson::Value& shader) const
+{
+    assert(!shader.IsNull());
+
+    PixelShaderGatewayMeta meta;
+    meta.m_shaderName = shader[NAME_TOKEN].GetString();
+    meta.m_api = m_apiReference.at(shader[API_TOKEN].GetString());
+    meta.m_filename = shader[FILE_TOKEN].GetString();
+    meta.m_profile = shader[PROFILE_TOKEN].GetString();
+    meta.m_entry = shader[ENTRY_TOKEN].GetString();
+    return meta;
 }
--- Source/Gateways/EffectPolicyJsonGateway.h
+++ Source/Gateways/EffectPolicyJsonGateway.h
@@ -11,6 +11,7 @@
 #define RAPIDJSON_HAS_STDSTRING 1
 
 #include "GameEngine/EffectCompilingPolicies.h"
+#include "GraphicKernel/IGraphicAPI.h"
 #include "rapidjson/document.h"
 #include <optional>
 
@@ -22,19 +23,39 @@ namespace Enigma::Gateways
         struct VertexShaderGatewayMeta
         {
             std::string m_shaderName;
+            Graphics::IGraphicAPI::APIVersion m_api;
+            std::string m_filename;
+            std::string m_profile;
+            std::string m_entry;
             std::string m_formatCode;
-            Engine::ShaderCodeProfile m_shaderCode;
             std::string m_layoutName;
+        };
+        struct PixelShaderGatewayMeta
+        {
+            std::string m_shaderName;
+            Graphics::IGraphicAPI::APIVersion m_api;
+            std::string m_filename;
+            std::string m_profile;
+            std::string m_entry;
         };
     public:
         EffectPolicyJsonGateway();
         std::optional<Engine::EffectCompilingPolicy> Deserialize(const std::string& json);
 
     private:
-        std::vector<VertexShaderGatewayMeta> DeserializeVertexShaderList(const rapidjson::Value& shader_list);
+        std::vector<VertexShaderGatewayMeta> DeserializeVertexShaderList(const rapidjson::Value& shader_list) const;
+        std::vector<PixelShaderGatewayMeta> DeserializePixelShaderList(const rapidjson::Value& shader_list) const;
+
+        VertexShaderGatewayMeta DeserializeVertexShaderMeta(const rapidjson::Value& shader) const;
+        PixelShaderGatewayMeta DeserializePixelShaderMeta(const rapidjson::Value& shader) const;
 
     private:
+        const std::unordered_map<std::string, Graphics::IGraphicAPI::APIVersion> m_apiReference {
+            {"dx9", Graphics::IGraphicAPI::APIVersion::API_Dx9}, {"dx11", Graphics::IGraphicAPI::APIVersion::API_Dx11},
+            {"egl", Graphics::IGraphicAPI::APIVersion::API_EGL}
+        };
         std::vector<VertexShaderGatewayMeta> m_vertexShaderGateways;
+        std::vector<PixelShaderGatewayMeta> m_pixelShaderGateways;
     };
 }
 

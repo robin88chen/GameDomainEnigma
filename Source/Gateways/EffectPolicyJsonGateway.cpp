a7fbd95db5c806201ddf847fd5dd6d753ab4d41c
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-09-15 18:01:36 +0800

    [Gateway][WIP] effect policy gateway : sampler state, rasterizer state

--- Source/Gateways/EffectPolicyJsonGateway.cpp
+++ Source/Gateways/EffectPolicyJsonGateway.cpp
@@ -14,6 +14,25 @@ using namespace Enigma::Engine;
 #define ENTRY_TOKEN	"Entry"
 #define VTX_DECL_TOKEN "VtxDecl"
 #define API_TOKEN	"API"
+#define SAMPLER_STATE_LIST_TOKEN "SamplerStateList"
+#define ADDRESS_U_TOKEN "AddressU"
+#define ADDRESS_V_TOKEN "AddressV"
+#define ADDRESS_W_TOKEN "AddressW"
+#define MIN_FILTER_TOKEN "MinFilter"
+#define MAG_FILTER_TOKEN "MagFilter"
+#define MIP_FILTER_TOKEN "MipFilter"
+#define COMPARE_FUNC_TOKEN "CmpFunc"
+#define BORDER_COLOR_TOKEN "BorderColor"
+#define ANISOTROPIC_TOKEN "Anisotropic"
+#define LOD_BIAS_TOKEN "LodBias"
+#define MIN_LOD_TOKEN "MinLod"
+#define MAX_LOD_TOKEN "MaxLod"
+#define RASTERIZER_STATE_LIST_TOKEN "RasterizerStateList"
+#define FILLMODE_TOKEN "FillMode"
+#define CULLMODE_TOKEN "CullMode"
+#define DEPTH_BIAS_TOKEN "DepthBias"
+#define DEPTH_BIAS_CLAMP_TOKEN "DepthBiasClamp"
+#define SLOPE_SCALE_DEPTH_BIAS_TOKEN "SlopeScaleDepthBias"
 
 EffectPolicyJsonGateway::EffectPolicyJsonGateway()
 {
@@ -31,6 +50,8 @@ std::optional<EffectCompilingPolicy> EffectPolicyJsonGateway::Deserialize(const 
     }
     m_vertexShaderGateways = DeserializeVertexShaderList(json_doc[VERTEX_SHADER_LIST_TOKEN]);
     m_pixelShaderGateways = DeserializePixelShaderList(json_doc[PIXEL_SHADER_LIST_TOKEN]);
+    m_samplerStateGateways = DeserializeSamplerStateList(json_doc[SAMPLER_STATE_LIST_TOKEN]);
+    m_rasterizerStateGateways = DeserializeRasterizerStateList(json_doc[RASTERIZER_STATE_LIST_TOKEN]);
 
     return std::nullopt;
 }
@@ -71,6 +92,40 @@ std::vector<EffectPolicyJsonGateway::PixelShaderGatewayMeta> EffectPolicyJsonGat
     return shader_metas;
 }
 
+std::vector<EffectPolicyJsonGateway::SamplerStateGatewayMeta> EffectPolicyJsonGateway::DeserializeSamplerStateList(const rapidjson::Value& sampler_list) const
+{
+    auto state_metas = std::vector<SamplerStateGatewayMeta>{};
+    if (FATAL_LOG_EXPR(!sampler_list.IsArray()))
+    {
+        std::string ss = string_format("sampler state list is not a array");
+        LOG(Info, ss);
+        return state_metas;
+    }
+    for (auto sampler = sampler_list.GetArray().Begin(); sampler != sampler_list.GetArray().End(); ++sampler)
+    {
+        state_metas.emplace_back(DeserializeSamplerStateMeta(*sampler));
+    }
+
+    return state_metas;
+}
+
+std::vector<EffectPolicyJsonGateway::RasterizerStateGatewayMeta> EffectPolicyJsonGateway::DeserializeRasterizerStateList(const rapidjson::Value& rasterizer_list) const
+{
+    auto state_metas = std::vector<RasterizerStateGatewayMeta>{};
+    if (FATAL_LOG_EXPR(!rasterizer_list.IsArray()))
+    {
+        std::string ss = string_format("rasterizer state list is not a array");
+        LOG(Info, ss);
+        return state_metas;
+    }
+    for (auto rasterizer = rasterizer_list.GetArray().Begin(); rasterizer != rasterizer_list.GetArray().End(); ++rasterizer)
+    {
+        state_metas.emplace_back(DeserializeRasterizerStateMeta(*rasterizer));
+    }
+
+    return state_metas;
+}
+
 EffectPolicyJsonGateway::VertexShaderGatewayMeta EffectPolicyJsonGateway::DeserializeVertexShaderMeta(const rapidjson::Value& shader) const
 {
     assert(!shader.IsNull());
@@ -98,3 +153,65 @@ EffectPolicyJsonGateway::PixelShaderGatewayMeta EffectPolicyJsonGateway::Deseria
     meta.m_entry = shader[ENTRY_TOKEN].GetString();
     return meta;
 }
+
+EffectPolicyJsonGateway::SamplerStateGatewayMeta EffectPolicyJsonGateway::DeserializeSamplerStateMeta(const rapidjson::Value& state) const
+{
+    assert(!state.IsNull());
+
+    SamplerStateGatewayMeta meta;
+    meta.m_name = state[NAME_TOKEN].GetString();
+    if (state.HasMember(ADDRESS_U_TOKEN))
+        meta.m_stateData.m_addressModeU = m_addressModeReference.at(state[ADDRESS_U_TOKEN].GetString());
+    if (state.HasMember(ADDRESS_V_TOKEN))
+        meta.m_stateData.m_addressModeV = m_addressModeReference.at(state[ADDRESS_V_TOKEN].GetString());
+    if (state.HasMember(ADDRESS_W_TOKEN))
+        meta.m_stateData.m_addressModeW = m_addressModeReference.at(state[ADDRESS_W_TOKEN].GetString());
+    if (state.HasMember(MAG_FILTER_TOKEN))
+        meta.m_stateData.m_magFilter = m_filterReference.at(state[MAG_FILTER_TOKEN].GetString());
+    if (state.HasMember(MIN_FILTER_TOKEN))
+        meta.m_stateData.m_minFilter = m_filterReference.at(state[MIN_FILTER_TOKEN].GetString());
+    if (state.HasMember(MIP_FILTER_TOKEN))
+        meta.m_stateData.m_mipFilter = m_filterReference.at(state[MIP_FILTER_TOKEN].GetString());
+    if (state.HasMember(COMPARE_FUNC_TOKEN))
+        meta.m_stateData.m_cmpFunc = m_compFuncReference.at(state[COMPARE_FUNC_TOKEN].GetString());
+    if (state.HasMember(BORDER_COLOR_TOKEN))
+        meta.m_stateData.m_borderColor = DeserializeColorRGBA(state[BORDER_COLOR_TOKEN]);
+    if (state.HasMember(ANISOTROPIC_TOKEN))
+        meta.m_stateData.m_levelAnisotropic = static_cast<unsigned char>(state[ANISOTROPIC_TOKEN].GetInt());
+    if (state.HasMember(LOD_BIAS_TOKEN))
+        meta.m_stateData.m_mipmapLODBias = state[LOD_BIAS_TOKEN].GetFloat();
+    if (state.HasMember(MIN_LOD_TOKEN))
+        meta.m_stateData.m_minLOD = state[MIN_LOD_TOKEN].GetFloat();
+    if (state.HasMember(MAX_LOD_TOKEN))
+        meta.m_stateData.m_maxLOD = state[MAX_LOD_TOKEN].GetFloat();
+
+    return meta;
+}
+
+EffectPolicyJsonGateway::RasterizerStateGatewayMeta EffectPolicyJsonGateway::DeserializeRasterizerStateMeta(const rapidjson::Value& state) const
+{
+    assert(!state.IsNull());
+
+    RasterizerStateGatewayMeta meta;
+    meta.m_name = state[NAME_TOKEN].GetString();
+    if (state.HasMember(FILLMODE_TOKEN))
+        meta.m_stateData.m_fillMode = m_fillModeReference.at(state[FILLMODE_TOKEN].GetString());
+    if (state.HasMember(CULLMODE_TOKEN))
+        meta.m_stateData.m_cullMode = m_cullModeReference.at(state[CULLMODE_TOKEN].GetString());
+    if (state.HasMember(DEPTH_BIAS_TOKEN))
+        meta.m_stateData.m_depthBias = state[DEPTH_BIAS_TOKEN].GetFloat();
+    if (state.HasMember(DEPTH_BIAS_CLAMP_TOKEN))
+        meta.m_stateData.m_depthBiasClamp = state[DEPTH_BIAS_CLAMP_TOKEN].GetFloat();
+    if (state.HasMember(SLOPE_SCALE_DEPTH_BIAS_TOKEN))
+        meta.m_stateData.m_slopeScaledDepthBias = state[SLOPE_SCALE_DEPTH_BIAS_TOKEN].GetFloat();
+
+    return meta;
+}
+
+Enigma::MathLib::ColorRGBA EffectPolicyJsonGateway::DeserializeColorRGBA(const rapidjson::Value& value) const
+{
+    if (!value.IsArray()) return MathLib::ColorRGBA{};
+    auto values = value.GetArray();
+    return MathLib::ColorRGBA{ values[0].GetFloat(), values[1].GetFloat(), values[2].GetFloat(), values[3].GetFloat() };
+}
+
--- Source/Gateways/EffectPolicyJsonGateway.h
+++ Source/Gateways/EffectPolicyJsonGateway.h
@@ -12,6 +12,11 @@
 
 #include "GameEngine/EffectCompilingPolicies.h"
 #include "GraphicKernel/IGraphicAPI.h"
+#include "GraphicKernel/IDeviceSamplerState.h"
+#include "GraphicKernel/IDeviceRasterizerState.h"
+#include "GraphicKernel/IDeviceAlphaBlendState.h"
+#include "GraphicKernel/IDeviceDepthStencilState.h"
+#include "MathLib/ColorRGBA.h"
 #include "rapidjson/document.h"
 #include <optional>
 
@@ -38,6 +43,26 @@ namespace Enigma::Gateways
             std::string m_profile;
             std::string m_entry;
         };
+        struct SamplerStateGatewayMeta
+        {
+            std::string m_name;
+            Graphics::IDeviceSamplerState::SamplerStateData m_stateData;
+        };
+        struct RasterizerStateGatewayMeta
+        {
+            std::string m_name;
+            Graphics::IDeviceRasterizerState::RasterizerStateData m_stateData;
+        };
+        struct BlendStateGatewayMeta
+        {
+            std::string m_name;
+            Graphics::IDeviceAlphaBlendState::BlendStateData m_stateData;
+        };
+        struct DepthStateGatewayMeta
+        {
+            std::string m_name;
+            Graphics::IDeviceDepthStencilState::DepthStencilData m_stateData;
+        };
     public:
         EffectPolicyJsonGateway();
         std::optional<Engine::EffectCompilingPolicy> Deserialize(const std::string& json);
@@ -45,17 +70,61 @@ namespace Enigma::Gateways
     private:
         std::vector<VertexShaderGatewayMeta> DeserializeVertexShaderList(const rapidjson::Value& shader_list) const;
         std::vector<PixelShaderGatewayMeta> DeserializePixelShaderList(const rapidjson::Value& shader_list) const;
+        std::vector<SamplerStateGatewayMeta> DeserializeSamplerStateList(const rapidjson::Value& sampler_list) const;
+        std::vector<RasterizerStateGatewayMeta> DeserializeRasterizerStateList(const rapidjson::Value& rasterizer_list) const;
+        std::vector<BlendStateGatewayMeta> DeserializeBlendStateList(const rapidjson::Value& sampler_list) const;
+        std::vector<DepthStateGatewayMeta> DeserializeDepthStateList(const rapidjson::Value& sampler_list) const;
 
         VertexShaderGatewayMeta DeserializeVertexShaderMeta(const rapidjson::Value& shader) const;
         PixelShaderGatewayMeta DeserializePixelShaderMeta(const rapidjson::Value& shader) const;
+        SamplerStateGatewayMeta DeserializeSamplerStateMeta(const rapidjson::Value& state) const;
+        RasterizerStateGatewayMeta DeserializeRasterizerStateMeta(const rapidjson::Value& state) const;
+        BlendStateGatewayMeta DeserializeBlendStateMeta(const rapidjson::Value& state) const;
+        DepthStateGatewayMeta DeserializeDepthStateMeta(const rapidjson::Value& state) const;
 
+        MathLib::ColorRGBA DeserializeColorRGBA(const rapidjson::Value& value) const;
     private:
         const std::unordered_map<std::string, Graphics::IGraphicAPI::APIVersion> m_apiReference {
             {"dx9", Graphics::IGraphicAPI::APIVersion::API_Dx9}, {"dx11", Graphics::IGraphicAPI::APIVersion::API_Dx11},
             {"egl", Graphics::IGraphicAPI::APIVersion::API_EGL}
         };
+        const std::unordered_map<std::string, Graphics::IDeviceSamplerState::SamplerStateData::Filter> m_filterReference {
+            {"None", Graphics::IDeviceSamplerState::SamplerStateData::Filter::None},
+            {"Linear", Graphics::IDeviceSamplerState::SamplerStateData::Filter::Linear},
+            {"Point", Graphics::IDeviceSamplerState::SamplerStateData::Filter::Point},
+        };
+        const std::unordered_map<std::string, Graphics::IDeviceSamplerState::SamplerStateData::AddressMode> m_addressModeReference {
+            {"Wrap", Graphics::IDeviceSamplerState::SamplerStateData::AddressMode::Wrap},
+            {"Clamp", Graphics::IDeviceSamplerState::SamplerStateData::AddressMode::Clamp},
+            {"Mirror", Graphics::IDeviceSamplerState::SamplerStateData::AddressMode::Mirror},
+            {"Border", Graphics::IDeviceSamplerState::SamplerStateData::AddressMode::Border},
+        };
+        const std::unordered_map<std::string, Graphics::IDeviceSamplerState::SamplerStateData::CompareFunc> m_compFuncReference{
+            {"Never", Graphics::IDeviceSamplerState::SamplerStateData::CompareFunc::Never},
+            {"Always", Graphics::IDeviceSamplerState::SamplerStateData::CompareFunc::Always},
+            {"Equal", Graphics::IDeviceSamplerState::SamplerStateData::CompareFunc::Equal},
+            {"Greater", Graphics::IDeviceSamplerState::SamplerStateData::CompareFunc::Greater},
+            {"GreaterEqual", Graphics::IDeviceSamplerState::SamplerStateData::CompareFunc::GreaterEqual},
+            {"Less", Graphics::IDeviceSamplerState::SamplerStateData::CompareFunc::Less},
+            {"LessEqual", Graphics::IDeviceSamplerState::SamplerStateData::CompareFunc::LessEqual},
+            {"NotEqual", Graphics::IDeviceSamplerState::SamplerStateData::CompareFunc::NotEqual},
+        };
+        const std::unordered_map<std::string, Graphics::IDeviceRasterizerState::RasterizerStateData::FillMode> m_fillModeReference{
+            {"Fill", Graphics::IDeviceRasterizerState::RasterizerStateData::FillMode::Fill},
+            {"Wireframe", Graphics::IDeviceRasterizerState::RasterizerStateData::FillMode::Wireframe},
+            {"Point", Graphics::IDeviceRasterizerState::RasterizerStateData::FillMode::Point},
+        };
+        const std::unordered_map<std::string, Graphics::IDeviceRasterizerState::RasterizerStateData::BackfaceCullMode> m_cullModeReference{
+            {"None", Graphics::IDeviceRasterizerState::RasterizerStateData::BackfaceCullMode::Cull_None},
+            {"CW", Graphics::IDeviceRasterizerState::RasterizerStateData::BackfaceCullMode::Cull_CW},
+            {"CCW", Graphics::IDeviceRasterizerState::RasterizerStateData::BackfaceCullMode::Cull_CCW},
+        };
         std::vector<VertexShaderGatewayMeta> m_vertexShaderGateways;
         std::vector<PixelShaderGatewayMeta> m_pixelShaderGateways;
+        std::vector<SamplerStateGatewayMeta> m_samplerStateGateways;
+        std::vector<RasterizerStateGatewayMeta> m_rasterizerStateGateways;
+        std::vector<BlendStateGatewayMeta> m_blendStateGateways;
+        std::vector<DepthStateGatewayMeta> m_depthStateGateways;
     };
 }
 

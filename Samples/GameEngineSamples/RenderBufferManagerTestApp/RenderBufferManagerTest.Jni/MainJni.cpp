998089f3efdf459e758dbf4c0d0fa97d51b31a55
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-09-23 20:30:30 +0800

    [Engine] render buffer manager test android app

---
+++ Samples/GameEngineSamples/RenderBufferManagerTestApp/RenderBufferManagerTest.Jni/MainJni.cpp
@@ -0,0 +1,26 @@
+﻿/**********************************
+ Java Native Interface library
+**********************************/
+#include <jni.h>
+#include "RenderBufferManagerTest.h"
+#include "FileSystem/FileSystem.h"
+#include "FileSystem/AndroidMountPath.h"
+
+RenderBufferManagerTest* g_app = new RenderBufferManagerTest("Render Buffer Manager Test App");
+
+/** This is the C++ implementation of the Java native method.
+@param env Pointer to JVM environment
+@param thiz Reference to Java this object
+*/
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_RenderBufferManagerTestApp_RenderBufferManagerTestApp_initBridgeCallback(JNIEnv * env, jobject thiz)
+{
+    g_app->InitBridgeCallback();
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_RenderBufferManagerTestApp_RenderBufferManagerTestApp_initAdditionalMountPath(JNIEnv * env, jobject thiz)
+{
+}

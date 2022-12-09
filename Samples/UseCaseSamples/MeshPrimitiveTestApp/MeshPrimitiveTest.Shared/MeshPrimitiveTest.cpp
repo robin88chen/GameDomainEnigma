4bad00dbd71b87ab3188f51f59fd3093187b94ca
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-12-09 09:02:36 +0800

    [Samples][WIP] mesh primitive test

---
+++ Samples/UseCaseSamples/MeshPrimitiveTestApp/MeshPrimitiveTest.Shared/MeshPrimitiveTest.cpp
@@ -0,0 +1,72 @@
+﻿#include "MeshPrimitiveTest.h"
+#include "FileSystem/FileSystem.h"
+#include "FileSystem/StdMountPath.h"
+#include "Controllers/InstallingPolicies.h"
+#include "CubeGeometryMaker.h"
+
+using namespace Enigma::FileSystem;
+using namespace Enigma::Controllers;
+
+std::string PrimaryTargetName = "primary_target";
+std::string DefaultRendererName = "default_renderer";
+
+MeshPrimitiveTest::MeshPrimitiveTest(const std::string app_name) : AppDelegate(app_name)
+{
+
+}
+
+MeshPrimitiveTest::~MeshPrimitiveTest()
+{
+
+}
+
+void MeshPrimitiveTest::InitializeMountPaths()
+{
+#if TARGET_PLATFORM == PLATFORM_WIN32
+    if (FileSystem::Instance())
+    {
+        auto path = std::filesystem::current_path();
+        auto mediaPath = path / "../../../../Media/";
+        FileSystem::Instance()->AddMountPath(std::make_shared<StdMountPath>(mediaPath.string(), MediaPathName));
+        FileSystem::Instance()->AddMountPath(std::make_shared<StdMountPath>(path.string(), "DataPath"));
+    }
+#elif TARGET_PLATFORM == PLATFORM_ANDROID
+    if (FileSystem::Instance())
+    {
+        FileSystem::Instance()->AddMountPath(std::make_shared<AndroidMountPath>("", MediaPathName));
+
+        std::string data_path = Enigma::Platforms::AndroidBridge::GetDataFilePath();
+        char s[2048];
+        memset(s, 0, 2048);
+        memcpy(s, data_path.c_str(), data_path.size());
+        FileSystem::Instance()->AddMountPath(std::make_shared<StdMountPath>(data_path, "DataPath"));
+    }
+#endif
+}
+
+void MeshPrimitiveTest::InstallEngine()
+{
+    assert(m_graphicMain);
+
+    auto creating_policy = std::make_unique<DeviceCreatingPolicy>(Enigma::Graphics::DeviceRequiredBits(), m_hwnd);
+    auto policy = std::make_unique<InstallingDefaultRendererPolicy>(std::move(creating_policy), DefaultRendererName, PrimaryTargetName);
+    m_graphicMain->InstallRenderEngine(std::move(policy));
+
+    CubeGeometryMaker::MakeSavedCube("test_geometry");
+}
+
+void MeshPrimitiveTest::ShutdownEngine()
+{
+    m_graphicMain->ShutdownRenderEngine();
+}
+
+void MeshPrimitiveTest::FrameUpdate()
+{
+    AppDelegate::FrameUpdate();
+}
+
+void MeshPrimitiveTest::RenderFrame()
+{
+
+}
+
---
+++ Samples/UseCaseSamples/MeshPrimitiveTestApp/MeshPrimitiveTest.Shared/MeshPrimitiveTest.h
@@ -0,0 +1,46 @@
+﻿/*********************************************************************
+ * \file   MeshPrimitiveTest.h
+ * \brief  
+ * 
+ * \author Lancelot 'Robin' Chen
+ * \date   December 2022
+ *********************************************************************/
+#ifndef _MESH_PRIMITIVE_TEST_H
+#define _MESH_PRIMITIVE_TEST_H
+
+#include "Platforms/PlatformConfig.h"
+#if TARGET_PLATFORM == PLATFORM_ANDROID
+#include "Application/AppDelegateAndroid.h"
+#else
+#include "Application/AppDelegateWin32.h"
+#endif
+#include "GameEngine/IRenderer.h"
+#include "Frameworks/Event.h"
+#include "Frameworks/EventSubscriber.h"
+#include "Frameworks/Timer.h"
+#include "Renderer/RenderTarget.h"
+#include "Renderer/RenderElement.h"
+#include "Renderer/Renderer.h"
+#include "GameEngine/RenderBuffer.h"
+#include "GameEngine/EffectMaterial.h"
+#include "GameEngine/EffectMaterialManager.h"
+
+class MeshPrimitiveTest : public Enigma::Application::AppDelegate
+{
+public:
+    static inline std::string MediaPathName = "APK_PATH";
+public:
+    MeshPrimitiveTest(const std::string app_name);
+    ~MeshPrimitiveTest();
+
+    virtual void InitializeMountPaths() override;
+
+    virtual void InstallEngine() override final;
+    virtual void ShutdownEngine() override final;
+
+    virtual void FrameUpdate() override;
+    virtual void RenderFrame() override;
+};
+
+
+#endif // _MESH_PRIMITIVE_TEST_H

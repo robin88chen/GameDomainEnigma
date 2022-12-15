063a1c97a29363527ef53ac0c57badff33bd4753
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2022-12-15 17:18:19 +0800

    [Renderer] mesh node & mesh node tree

---
+++ Source/Renderer/MeshNode.h
@@ -0,0 +1,48 @@
+﻿/*********************************************************************
+ * \file   MeshNode.h
+ * \brief  Mesh Node, value object, used by value
+ * 
+ * \author Lancelot 'Robin' Chen
+ * \date   December 2022
+ *********************************************************************/
+#ifndef _MESH_NODE_H
+#define _MESH_NODE_H
+
+#include "MeshPrimitive.h"
+#include "MathLib/Matrix4.h"
+#include <string>
+
+namespace Enigma::Renderer
+{
+    class MeshNode
+    {
+    public:
+        MeshNode(const std::string& name);
+        MeshNode(const MeshNode& node);
+        MeshNode(MeshNode&& node);
+        ~MeshNode();
+        MeshNode& operator=(const MeshNode& node);
+        MeshNode& operator=(MeshNode&& node);
+
+        const std::string& GetName() const { return m_name; }
+        void SetName(const std::string& name) { m_name = name; }
+        const MathLib::Matrix4& GetLocalTransform() const { return m_mxLocalTransform; }
+        void SetLocalTransform(const MathLib::Matrix4& mx) { m_mxLocalTransform = mx; }
+        const MathLib::Matrix4& GetRootRefTransform() const { return m_mxRootRefTransform; }
+        void SetRootRefTransform(const MathLib::Matrix4& mx) { m_mxRootRefTransform = mx; }
+        const MeshPrimitivePtr& GetMeshPrimitive() const { return m_meshPrimitive; }
+        MeshPrimitivePtr GetMeshPrimitive() { return m_meshPrimitive; }
+        void SetMeshPrimitive(const MeshPrimitivePtr& mesh);
+        std::optional<unsigned> GetParentIndexInArray() const { return m_parentIndexInArray; }
+        void SetParentIndexIndexInArray(unsigned idx);
+
+    protected:
+        std::string m_name;
+        MathLib::Matrix4 m_mxLocalTransform;   ///< mesh node local transform (reference: parent node)
+        MathLib::Matrix4 m_mxRootRefTransform; ///< mesh node transform (reference: model root)
+        MeshPrimitivePtr m_meshPrimitive;   ///< mesh primitive
+        std::optional<unsigned> m_parentIndexInArray;  ///< parent index in mesh node array
+    };
+}
+
+#endif // _MESH_NODE_H

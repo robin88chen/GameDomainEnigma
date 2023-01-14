2481404bf8511646559ee2a023c4e733b960ac89
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2023-01-14 16:30:28 +0800

    [TimerService] add timer service

---
+++ Source/GameEngine/TimerService.cpp
@@ -0,0 +1,70 @@
+﻿#include "TimerService.h"
+#include "Frameworks/Timer.h"
+
+using namespace Enigma::Frameworks;
+using namespace Enigma::Engine;
+
+DEFINE_RTTI(Engine, TimerService, ISystemService);
+
+TimerService::TimerService(ServiceManager* manager) : ISystemService(manager)
+{
+    m_needTick = true;
+    m_orderValue = TimerServiceOrderValue;
+    m_gameTimer = std::make_unique<Timer>();
+    m_realLifeTimer = std::make_unique<Timer>();
+}
+
+TimerService::~TimerService()
+{
+    m_gameTimer = nullptr;
+    m_realLifeTimer = nullptr;
+}
+
+ServiceResult TimerService::OnInit()
+{
+    if (m_gameTimer) m_gameTimer->Reset();
+    if (m_realLifeTimer) m_realLifeTimer->Reset();
+
+    return ServiceResult::Complete;
+}
+
+ServiceResult TimerService::OnTick()
+{
+    if (m_gameTimer) m_gameTimer->Update();
+    if (m_realLifeTimer) m_realLifeTimer->Update();
+
+    return ServiceResult::Pendding;
+}
+
+ServiceResult TimerService::OnTerm()
+{
+    m_gameTimer = nullptr;
+    m_realLifeTimer = nullptr;
+
+    return ServiceResult::Complete;
+}
+
+void TimerService::SetGameTimerScale(float scale)
+{
+    if (m_gameTimer) m_gameTimer->SetScale(scale);
+}
+
+void TimerService::PauseGameTimer()
+{
+    if (m_gameTimer) m_gameTimer->Pause();
+}
+
+void TimerService::ResumeGameTimer()
+{
+    if (m_gameTimer) m_gameTimer->Resume();
+}
+
+void TimerService::ReverseGameTimer()
+{
+    if (m_gameTimer) m_gameTimer->SetScale(-m_gameTimer->GetScale());
+}
+
+void TimerService::SetGameTimerStep(bool enable, float step)
+{
+    if (m_gameTimer) m_gameTimer->SetFrameStep(enable, step);
+}
---
+++ Source/GameEngine/TimerService.h
@@ -0,0 +1,50 @@
+﻿/*********************************************************************
+ * \file   TimerService.h
+ * \brief  
+ * 
+ * \author Lancelot 'Robin' Chen
+ * \date   January 2023
+ *********************************************************************/
+#ifndef _TIMER_SERVICE_H
+#define _TIMER_SERVICE_H
+
+#include "Frameworks/SystemService.h"
+#include "Frameworks/Timer.h"
+#include <memory>
+
+namespace Enigma::Engine
+{
+    class TimerService : public Frameworks::ISystemService
+    {
+        DECLARE_EN_RTTI;
+    public:
+        TimerService(Frameworks::ServiceManager* manager);
+        TimerService(const TimerService&) = delete;
+        virtual ~TimerService();
+        TimerService& operator=(const TimerService&) = delete;
+
+        virtual Frameworks::ServiceResult OnInit() override;
+        virtual Frameworks::ServiceResult OnTick() override;
+        virtual Frameworks::ServiceResult OnTerm() override;
+
+        const std::unique_ptr<Frameworks::Timer>& GetGameTimer() { return m_gameTimer; };
+        const std::unique_ptr<Frameworks::Timer>& GetRealLifeTimer() { return m_realLifeTimer; };
+
+        /// speed up : scale > 1.0, slow down : scale < 1.0, pause : scale = 0.0 
+        void SetGameTimerScale(float scale);
+        /// time step ( in seconds ) for every frame
+        void SetGameTimerStep(bool enable, float step);
+
+        void PauseGameTimer();
+        void ResumeGameTimer();
+
+        /// reverse timer
+        void ReverseGameTimer();
+
+    protected:
+        std::unique_ptr<Frameworks::Timer> m_gameTimer;
+        std::unique_ptr<Frameworks::Timer> m_realLifeTimer;
+    };
+};
+
+#endif // _TIMER_SERVICE_H

bbd4a94a2aa12ecaf75db0ececc2962d01edb7c4
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2023-02-03 18:12:14 +0800

    [SkinMeshTest] win32 app 建立

---
+++ Samples/UseCaseSamples/SkinMeshPrimitiveTestApp/SkinMeshPrimitiveTest.Shared/SkinMeshPrimitiveTest.cpp
@@ -0,0 +1,62 @@
+﻿#include "SkinMeshPrimitiveTest.h"
+#include <string>
+
+using namespace Enigma::Controllers;
+using namespace Enigma::Application;
+using namespace Enigma::Frameworks;
+
+SkinMeshPrimitiveTest::SkinMeshPrimitiveTest(const std::string app_name) : AppDelegate(app_name)
+{
+
+}
+
+SkinMeshPrimitiveTest::~SkinMeshPrimitiveTest()
+{
+
+}
+
+void SkinMeshPrimitiveTest::InitializeMountPaths()
+{
+
+}
+
+void SkinMeshPrimitiveTest::InstallEngine()
+{
+
+}
+
+void SkinMeshPrimitiveTest::ShutdownEngine()
+{
+
+}
+
+void SkinMeshPrimitiveTest::FrameUpdate()
+{
+
+}
+
+void SkinMeshPrimitiveTest::RenderFrame()
+{
+
+}
+
+void SkinMeshPrimitiveTest::OnRendererCreated(const IEventPtr& e)
+{
+
+}
+
+void SkinMeshPrimitiveTest::OnRenderTargetCreated(const IEventPtr& e)
+{
+
+}
+
+void SkinMeshPrimitiveTest::OnRenderablePrimitiveBuilt(const IEventPtr& e)
+{
+
+}
+
+void SkinMeshPrimitiveTest::OnBuildRenderablePrimitiveFailed(const IEventPtr& e)
+{
+
+}
+

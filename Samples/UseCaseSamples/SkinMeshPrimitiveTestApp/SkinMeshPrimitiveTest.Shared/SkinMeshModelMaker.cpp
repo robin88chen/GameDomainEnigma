1d4c0ddce3b0291cf985a74b7040eb34a72f2337
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2023-02-04 16:06:22 +0800

    [SkinMeshTest] 建立 mesh & model

---
+++ Samples/UseCaseSamples/SkinMeshPrimitiveTestApp/SkinMeshPrimitiveTest.Shared/SkinMeshModelMaker.cpp
@@ -0,0 +1,69 @@
+﻿#include "SkinMeshModelMaker.h"
+#include "Renderer/RenderablePrimitiveDtos.h"
+#include "Frameworks/StringFormat.h"
+#include "Gateways/DtoJsonGateway.h"
+#include "Gateways/JsonFileDtoDeserializer.h"
+#include "Gateways/JsonFileEffectProfileDeserializer.h"
+#include "GameEngine/TriangleList.h"
+
+using namespace Enigma::Renderer;
+using namespace Enigma::MathLib;
+using namespace Enigma::Gateways;
+using namespace Enigma::Engine;
+
+std::vector<std::string> bone_node_names{ "bone_root", "bone_one" };
+
+std::shared_ptr<ModelPrimitivePolicy> SkinMeshModelMaker::MakeModelPrimitivePolicy(const std::string& model_name, const std::string& geo_name)
+{
+    MeshNodeTreeDto tree;
+    MeshNodeDto root_node_dto;
+    root_node_dto.Name() = bone_node_names[0];
+    root_node_dto.LocalTransform() = Matrix4::MakeIdentity();
+    root_node_dto.TheMeshPrimitive() = MakeSkinMeshPrimitiveDto(model_name + "_skinmesh", geo_name).ToGenericDto();
+    tree.MeshNodes().emplace_back(root_node_dto.ToGenericDto());
+    MeshNodeDto bone_node_dto;
+    bone_node_dto.Name() = bone_node_names[1];
+    bone_node_dto.LocalTransform() = Matrix4::MakeTranslateTransform(Vector3(0.0f, 2.0f, 0.0f));
+    bone_node_dto.ParentIndexInArray() = static_cast<unsigned>(tree.MeshNodes().size() - 1);
+    tree.MeshNodes().emplace_back(bone_node_dto.ToGenericDto());
+
+    ModelPrimitiveDto dto;
+    dto.Name() = model_name;
+    dto.TheNodeTree() = tree.ToGenericDto();
+    /*auto animation = ModelAnimatorMaker::MakeModelAnimationAsset(model_name, node_names);
+    auto animator = ModelAnimatorMaker::MakeModelAnimator(model_name, animation);
+    dto.TheAnimator() = animator->SerializeDto().ToGenericDto();*/
+    return dto.ConvertToPolicy(std::make_shared<JsonFileDtoDeserializer>(),
+        std::make_shared<JsonFileEffectProfileDeserializer>());
+}
+
+SkinMeshPrimitiveDto SkinMeshModelMaker::MakeSkinMeshPrimitiveDto(const std::string& mesh_name, const std::string& geo_name)
+{
+    SkinMeshPrimitiveDto dto;
+    dto.Name() = mesh_name;
+    dto.GeometryName() = geo_name;
+    dto.GeometryFactoryDesc() = FactoryDesc(TriangleList::TYPE_RTTI.GetName()).ClaimFromResource(geo_name, geo_name + ".geo", "DataPath");
+    dto.Effects().emplace_back(MakeEffectDto("skinned_vtx_nor_tex").ToGenericDto());
+    dto.TextureMaps().emplace_back(MakeTextureMapDto().ToGenericDto());
+    return dto;
+}
+
+EffectMaterialDto SkinMeshModelMaker::MakeEffectDto(const std::string& eff_name)
+{
+    EffectMaterialDto dto;
+    dto.Name() = eff_name;
+    dto.TheFactoryDesc() = FactoryDesc(EffectMaterial::TYPE_RTTI.GetName()).ClaimFromResource(eff_name, eff_name + ".efx", "APK_PATH");
+    return dto;
+}
+
+EffectTextureMapDto SkinMeshModelMaker::MakeTextureMapDto()
+{
+    EffectTextureMapDto dto;
+    TextureMappingDto tex;
+    tex.Filename() = "earth.png";
+    tex.Semantic() = "DiffuseMap";
+    tex.TextureName() = "earth";
+    tex.PathId() = "APK_PATH";
+    dto.TextureMappings().emplace_back(tex);
+    return dto;
+}
--- Samples/UseCaseSamples/SkinMeshPrimitiveTestApp/SkinMeshPrimitiveTest.Shared/SkinMeshPrimitiveTest.cpp
+++ Samples/UseCaseSamples/SkinMeshPrimitiveTestApp/SkinMeshPrimitiveTest.Shared/SkinMeshPrimitiveTest.cpp
@@ -1,13 +1,30 @@
 ﻿#include "SkinMeshPrimitiveTest.h"
+#include "FileSystem/FileSystem.h"
+#include "FileSystem/StdMountPath.h"
+#include "Frameworks/EventPublisher.h"
+#include "Renderer/RenderablePrimitiveEvents.h"
+#include "Renderer/RenderablePrimitiveCommands.h"
+#include "Renderer/RendererEvents.h"
+#include "Controllers/GraphicMain.h"
+#include "Controllers/InstallingPolicies.h"
+#include "Frameworks/CommandBus.h"
+#include "CubeGeometryMaker.h"
+#include "CameraMaker.h"
+#include "SkinMeshModelMaker.h"
 #include <string>
 
 using namespace Enigma::Controllers;
 using namespace Enigma::Application;
 using namespace Enigma::Frameworks;
+using namespace Enigma::FileSystem;
+using namespace Enigma::Renderer;
+
+std::string PrimaryTargetName = "primary_target";
+std::string DefaultRendererName = "default_renderer";
 
 SkinMeshPrimitiveTest::SkinMeshPrimitiveTest(const std::string app_name) : AppDelegate(app_name)
 {
-
+    m_isPrefabBuilt = false;
 }
 
 SkinMeshPrimitiveTest::~SkinMeshPrimitiveTest()
@@ -17,11 +34,50 @@ SkinMeshPrimitiveTest::~SkinMeshPrimitiveTest()
 
 void SkinMeshPrimitiveTest::InitializeMountPaths()
 {
+#if TARGET_PLATFORM == PLATFORM_WIN32
+    if (FileSystem::Instance())
+    {
+        auto path = std::filesystem::current_path();
+        auto mediaPath = path / "../../../../Media/";
+        FileSystem::Instance()->AddMountPath(std::make_shared<StdMountPath>(mediaPath.string(), MediaPathName));
+        FileSystem::Instance()->AddMountPath(std::make_shared<StdMountPath>(path.string(), "DataPath"));
+    }
+#elif TARGET_PLATFORM == PLATFORM_ANDROID
+    if (FileSystem::Instance())
+    {
+        FileSystem::Instance()->AddMountPath(std::make_shared<AndroidMountPath>("", MediaPathName));
 
+        std::string data_path = Enigma::Platforms::AndroidBridge::GetDataFilePath();
+        char s[2048];
+        memset(s, 0, 2048);
+        memcpy(s, data_path.c_str(), data_path.size());
+        FileSystem::Instance()->AddMountPath(std::make_shared<StdMountPath>(data_path, "DataPath"));
+    }
+#endif
 }
 
 void SkinMeshPrimitiveTest::InstallEngine()
 {
+    m_onRenderablePrimitiveBuilt = std::make_shared<EventSubscriber>([=](auto e) {this->OnRenderablePrimitiveBuilt(e); });
+    EventPublisher::Subscribe(typeid(RenderablePrimitiveBuilt), m_onRenderablePrimitiveBuilt);
+    m_onBuildRenderablePrimitiveFailed = std::make_shared<EventSubscriber>([=](auto e) {this->OnBuildRenderablePrimitiveFailed(e); });
+    EventPublisher::Subscribe(typeid(BuildRenderablePrimitiveFailed), m_onBuildRenderablePrimitiveFailed);
+    m_onRendererCreated = std::make_shared<EventSubscriber>([=](auto e) {this->OnRendererCreated(e); });
+    EventPublisher::Subscribe(typeid(RendererCreated), m_onRendererCreated);
+    m_onRenderTargetCreated = std::make_shared<EventSubscriber>([=](auto e) {this->OnRenderTargetCreated(e); });
+    EventPublisher::Subscribe(typeid(PrimaryRenderTargetCreated), m_onRenderTargetCreated);
+
+    assert(m_graphicMain);
+
+    auto creating_policy = std::make_unique<DeviceCreatingPolicy>(Enigma::Graphics::DeviceRequiredBits(), m_hwnd);
+    auto policy = std::make_unique<InstallingDefaultRendererPolicy>(std::move(creating_policy), DefaultRendererName, PrimaryTargetName);
+    m_graphicMain->InstallRenderEngine(std::move(policy));
+
+    CubeGeometryMaker::MakeSavedCube("test_geometry");
+    auto prim_policy = SkinMeshModelMaker::MakeModelPrimitivePolicy("test_model", "test_geometry");
+    CommandBus::Post(std::make_shared<Enigma::Renderer::BuildRenderablePrimitive>(prim_policy));
+
+    m_camera = CameraMaker::MakeCamera();
 
 }
 

daae1553b85a63ca74fa629f4d5e5b30c876af4d
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2023-03-10 17:37:20 +0800

    [PortalSystemTest] portal system test app

---
+++ Samples/UseCaseSamples/PortalSystemTestApp/PortalSystemTest.Shared/PortalSystemTest.cpp
@@ -0,0 +1,131 @@
+﻿#include "PortalSystemTest.h"
+#include "FileSystem/FileSystem.h"
+#include "FileSystem/StdMountPath.h"
+#include "FileSystem/AndroidMountPath.h"
+#include "Platforms/AndroidBridge.h"
+#include "Frameworks/EventSubscriber.h"
+#include "Frameworks/EventPublisher.h"
+#include "Frameworks/CommandBus.h"
+#include "Renderer/RendererEvents.h"
+#include "SceneGraph/SceneGraphEvents.h"
+#include "SceneGraph/SceneGraphCommands.h"
+#include "SceneGraph/Culler.h"
+#include "Controllers/InstallingPolicies.h"
+#include "Gateways/JsonFileDtoDeserializer.h"
+#include "Gateways/JsonFileEffectProfileDeserializer.h"
+#include "Platforms/MemoryAllocMacro.h"
+#include "Platforms/MemoryMacro.h"
+
+using namespace Enigma::Application;
+using namespace Enigma::FileSystem;
+using namespace Enigma::Frameworks;
+using namespace Enigma::Renderer;
+using namespace Enigma::SceneGraph;
+using namespace Enigma::Controllers;
+using namespace Enigma::Gateways;
+using namespace Enigma::Engine;
+
+std::string PrimaryTargetName = "primary_target";
+std::string DefaultRendererName = "default_renderer";
+
+PortalSystemTest::PortalSystemTest(const std::string app_name) : AppDelegate(app_name)
+{
+    m_culler = nullptr;
+}
+
+PortalSystemTest::~PortalSystemTest()
+{
+}
+
+void PortalSystemTest::InitializeMountPaths()
+{
+#if TARGET_PLATFORM == PLATFORM_WIN32
+    if (FileSystem::Instance())
+    {
+        auto path = std::filesystem::current_path();
+        auto mediaPath = path / "../../../../Media/";
+        FileSystem::Instance()->AddMountPath(std::make_shared<StdMountPath>(mediaPath.string(), MediaPathName));
+        FileSystem::Instance()->AddMountPath(std::make_shared<StdMountPath>(path.string(), "DataPath"));
+    }
+#elif TARGET_PLATFORM == PLATFORM_ANDROID
+    if (FileSystem::Instance())
+    {
+        FileSystem::Instance()->AddMountPath(std::make_shared<AndroidMountPath>("", MediaPathName));
+
+        std::string data_path = Enigma::Platforms::AndroidBridge::GetDataFilePath();
+        char s[2048];
+        memset(s, 0, 2048);
+        memcpy(s, data_path.c_str(), data_path.size());
+        FileSystem::Instance()->AddMountPath(std::make_shared<StdMountPath>(data_path, "DataPath"));
+    }
+#endif
+}
+
+void PortalSystemTest::InstallEngine()
+{
+    m_onRendererCreated = std::make_shared<EventSubscriber>([=](auto e) { this->OnRendererCreated(e); });
+    m_onRenderTargetCreated = std::make_shared<EventSubscriber>([=](auto e) { this->OnRenderTargetCreated(e); });
+    EventPublisher::Subscribe(typeid(RendererCreated), m_onRendererCreated);
+    EventPublisher::Subscribe(typeid(PrimaryRenderTargetCreated), m_onRenderTargetCreated);
+
+    assert(m_graphicMain);
+
+    auto creating_policy = std::make_shared<DeviceCreatingPolicy>(Enigma::Graphics::DeviceRequiredBits(), m_hwnd);
+    auto renderer_policy = std::make_shared<InstallingDefaultRendererPolicy>(DefaultRendererName, PrimaryTargetName);
+    auto scene_graph_policy = std::make_shared<SceneGraphBuildingPolicy>(
+        std::make_shared<JsonFileDtoDeserializer>(), std::make_shared<JsonFileEffectProfileDeserializer>());
+    m_graphicMain->InstallRenderEngine({ creating_policy, renderer_policy, scene_graph_policy });
+}
+
+void PortalSystemTest::ShutdownEngine()
+{
+    m_pawn = nullptr;
+    m_sceneRoot = nullptr;
+    m_model = nullptr;
+    m_renderer = nullptr;
+    m_renderTarget = nullptr;
+    m_camera = nullptr;
+    SAFE_DELETE(m_culler);
+    EventPublisher::Unsubscribe(typeid(RendererCreated), m_onRendererCreated);
+    EventPublisher::Unsubscribe(typeid(PrimaryRenderTargetCreated), m_onRenderTargetCreated);
+    m_onRendererCreated = nullptr;
+    m_onRenderTargetCreated = nullptr;
+    m_onSceneGraphBuilt = nullptr;
+    m_onPawnPrimitiveBuilt = nullptr;
+
+    m_graphicMain->ShutdownRenderEngine();
+}
+
+void PortalSystemTest::FrameUpdate()
+{
+    AppDelegate::FrameUpdate();
+}
+
+void PortalSystemTest::RenderFrame()
+{
+    if (!m_renderer) return;
+    m_renderer->BeginScene();
+    m_renderer->ClearRenderTarget();
+    m_renderer->DrawScene();
+    m_renderer->EndScene();
+    m_renderer->Flip();
+}
+
+void PortalSystemTest::OnRendererCreated(const IEventPtr& e)
+{
+    if (!e) return;
+    const auto ev = std::dynamic_pointer_cast<RendererCreated, IEvent>(e);
+    if (!ev) return;
+    m_renderer = std::dynamic_pointer_cast<Renderer, IRenderer>(ev->GetRenderer());
+    m_renderer->SetAssociatedCamera(m_camera);
+    if ((m_renderer) && (m_renderTarget)) m_renderer->SetRenderTarget(m_renderTarget);
+}
+
+void PortalSystemTest::OnRenderTargetCreated(const IEventPtr& e)
+{
+    if (!e) return;
+    const auto ev = std::dynamic_pointer_cast<PrimaryRenderTargetCreated, IEvent>(e);
+    if (!ev) return;
+    m_renderTarget = ev->GetRenderTarget();
+    if ((m_renderer) && (m_renderTarget)) m_renderer->SetRenderTarget(m_renderTarget);
+}
---
+++ Samples/UseCaseSamples/PortalSystemTestApp/PortalSystemTest.Shared/PortalSystemTest.h
@@ -0,0 +1,73 @@
+﻿/*********************************************************************
+ * \file   PortalSystemTest.h
+ * \brief  
+ * 
+ * \author Lancelot 'Robin' Chen
+ * \date   March 2023
+ *********************************************************************/
+#ifndef _PORTAL_SYSTEM_TEST_H
+#define _PORTAL_SYSTEM_TEST_H
+
+#include "Platforms/PlatformConfig.h"
+#if TARGET_PLATFORM == PLATFORM_ANDROID
+#include "Application/AppDelegateAndroid.h"
+#else
+#include "Application/AppDelegateWin32.h"
+#endif
+#include "GameEngine/IRenderer.h"
+#include "Frameworks/Event.h"
+#include "Frameworks/EventSubscriber.h"
+#include "Frameworks/Timer.h"
+#include "Renderer/ModelPrimitive.h"
+#include "Renderer/RenderTarget.h"
+#include "Renderer/Renderer.h"
+#include "SceneGraph/Node.h"
+#include "SceneGraph/Pawn.h"
+#include <memory>
+#include <string>
+
+class PortalSystemTest : public Enigma::Application::AppDelegate
+{
+public:
+    static inline std::string MediaPathName = "APK_PATH";
+public:
+    PortalSystemTest(const std::string app_name);
+    ~PortalSystemTest() override;
+
+    virtual void InitializeMountPaths() override;
+
+    virtual void InstallEngine() override final;
+    virtual void ShutdownEngine() override final;
+
+    virtual void FrameUpdate() override;
+    virtual void RenderFrame() override;
+
+protected:
+    void OnRendererCreated(const Enigma::Frameworks::IEventPtr& e);
+    void OnRenderTargetCreated(const Enigma::Frameworks::IEventPtr& e);
+    void OnSceneGraphBuilt(const Enigma::Frameworks::IEventPtr& e);
+    void OnPawnPrimitiveBuilt(const Enigma::Frameworks::IEventPtr& e);
+
+private:
+    void RetrieveDtoCreatedModel();
+    void InsertDtoCreatedModelToRenderer();
+
+    void PrepareRenderScene();
+
+protected:
+    Enigma::Frameworks::EventSubscriberPtr m_onRendererCreated;
+    Enigma::Frameworks::EventSubscriberPtr m_onRenderTargetCreated;
+    Enigma::Frameworks::EventSubscriberPtr m_onSceneGraphBuilt;
+    Enigma::Frameworks::EventSubscriberPtr m_onPawnPrimitiveBuilt;
+
+    Enigma::Renderer::RendererPtr m_renderer;
+    Enigma::Renderer::RenderTargetPtr m_renderTarget;
+    Enigma::Renderer::ModelPrimitivePtr m_model;
+    Enigma::SceneGraph::NodePtr m_sceneRoot;
+    Enigma::SceneGraph::PawnPtr m_pawn;
+
+    std::shared_ptr<Enigma::SceneGraph::Camera> m_camera;
+    Enigma::SceneGraph::Culler* m_culler;
+};
+
+#endif // _PORTAL_SYSTEM_TEST_H

af19d8d726fd21629390c2d8c4b0361c9ecd0b1a
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2023-11-28 11:41:09 +0800

    [SceneGraph] add spatial id, scene graph store mapper

---
+++ Source/SceneGraph/SceneGraphStoreMapper.h
@@ -0,0 +1,25 @@
+﻿/*********************************************************************
+ * \file   SceneGraphStoreMapper.h
+ * \brief  scene graph store mapper interface, used to map the store (e.g. database)
+ *
+ * \author Lancelot 'Robin' Chen
+ * \date   November 2023
+ *********************************************************************/
+#ifndef SCENE_GRAPH_STORE_MAPPER_H
+#define SCENE_GRAPH_STORE_MAPPER_H
+
+#include <system_error>
+
+namespace Enigma::SceneGraph
+{
+    class SceneGraphStoreMapper
+    {
+    public:
+        virtual ~SceneGraphStoreMapper() = default;
+
+        virtual std::error_code connect() = 0;
+        virtual std::error_code disconnect() = 0;
+    };
+}
+
+#endif // SCENE_GRAPH_STORE_MAPPER_H
---
+++ Source/SceneGraph/SpatialId.h
@@ -0,0 +1,38 @@
+﻿/*********************************************************************
+ * \file   SpatialId.h
+ * \brief  scene graph spatial id, used to identify a spatial object
+ *
+ * \author Lancelot 'Robin' Chen
+ * \date   November 2023
+ *********************************************************************/
+#ifndef SPATIAL_ID_H
+#define SPATIAL_ID_H
+
+#include "Frameworks/Rtti.h"
+#include <string>
+
+namespace Enigma::SceneGraph
+{
+    class SpatialId
+    {
+    public:
+        SpatialId(const std::string& name, const Frameworks::Rtti& rtti);
+        ~SpatialId();
+        SpatialId(const SpatialId& other);
+        SpatialId& operator=(const SpatialId& other);
+        SpatialId(SpatialId&& other);
+        SpatialId& operator=(SpatialId&& other);
+
+        bool operator==(const SpatialId& other) const { return m_name == other.m_name && m_rtti == other.m_rtti; }
+        bool operator!=(const SpatialId& other) const { return m_name != other.m_name || m_rtti != other.m_rtti; }
+
+        const std::string& name() const { return m_name; }
+        const Frameworks::Rtti& rtti() const { return *m_rtti; }
+
+    private:
+        std::string m_name;
+        const Frameworks::Rtti* m_rtti;
+    };
+}
+
+#endif // SPATIAL_ID_H

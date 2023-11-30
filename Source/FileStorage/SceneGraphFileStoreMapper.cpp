d60ce645148fb47da27da0aa6fbb45bf07dfb2ef
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2023-11-30 11:41:46 +0800

    [SceneGraphFileStoreMapper] implement connect / disconnect

--- Source/FileStorage/SceneGraphFileStoreMapper.cpp
+++ Source/FileStorage/SceneGraphFileStoreMapper.cpp
@@ -1,14 +1,35 @@
 ﻿#include "SceneGraphFileStoreMapper.h"
+#include "FileSystem/FileSystem.h"
+#include "FileSystem/FileSystemErrors.h"
+#include <cassert>
 
 using namespace Enigma::FileStorage;
 
-SceneGraphFileStoreMapper::SceneGraphFileStoreMapper() : SceneGraph::SceneGraphStoreMapper()
+SceneGraphFileStoreMapper::SceneGraphFileStoreMapper(const std::string& mapper_filename) : SceneGraph::SceneGraphStoreMapper()
 {
-
+    m_mapper_filename = mapper_filename;
 }
 
 SceneGraphFileStoreMapper::~SceneGraphFileStoreMapper()
 {
+    assert(m_filename_map.empty());
+}
 
+std::error_code SceneGraphFileStoreMapper::connect()
+{
+    m_filename_map.clear();
+    FileSystem::IFilePtr mapper_file = FileSystem::FileSystem::Instance()->OpenFile(m_mapper_filename, FileSystem::ReadWriteOptionRead);
+    if (!mapper_file) return FileSystem::ErrorCode::ok;
+    auto file_size = mapper_file->Size();
+    auto content = mapper_file->Read(0, file_size);
+    if (!content) return FileSystem::ErrorCode::readFail;
+    parseMapperFile(std::string(content->begin(), content->end()));
+    return FileSystem::ErrorCode::ok;
+}
+
+std::error_code SceneGraphFileStoreMapper::disconnect()
+{
+    m_filename_map.clear();
+    return FileSystem::ErrorCode::ok;
 }
 
--- Source/FileStorage/SceneGraphFileStoreMapper.h
+++ Source/FileStorage/SceneGraphFileStoreMapper.h
@@ -15,8 +15,22 @@ namespace Enigma::FileStorage
     class SceneGraphFileStoreMapper : public SceneGraph::SceneGraphStoreMapper
     {
     public:
-        SceneGraphFileStoreMapper();
-        ~SceneGraphFileStoreMapper();
+        SceneGraphFileStoreMapper(const std::string& mapper_filename);
+        ~SceneGraphFileStoreMapper() override;
+        virtual std::error_code connect() override;
+        virtual std::error_code disconnect() override;
+
+        virtual bool hasCamera(const SceneGraph::SpatialId& id) override;
+        virtual std::shared_ptr<SceneGraph::Camera> queryCamera(const SceneGraph::SpatialId& id) override;
+        virtual std::error_code removeCamera(const SceneGraph::SpatialId& id) override;
+        virtual std::error_code serializeCamera(const SceneGraph::SpatialId& id, const std::shared_ptr<SceneGraph::Camera>& camera) override;
+
+    protected:
+        void parseMapperFile(const std::string& mapper_file_content);
+
+    protected:
+        std::string m_mapper_filename;
+        std::unordered_map<SceneGraph::SpatialId, std::string, SceneGraph::SpatialId::hash> m_filename_map;
     };
 }
 

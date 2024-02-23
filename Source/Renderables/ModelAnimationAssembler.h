cebedeae622893129a1b55a95760b540b642b25b
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2024-02-23 08:51:35 +0800

    [Renderables] model animation assembler

---
+++ Source/Renderables/ModelAnimationAssembler.h
@@ -0,0 +1,49 @@
+﻿/*********************************************************************
+ * \file   ModelAnimationAssembler.h
+ * \brief
+ *
+ * \author Lancelot 'Robin' Chen
+ * \date   February 2024
+ *********************************************************************/
+#ifndef MODEL_ANIMATION_ASSEMBLER_H
+#define MODEL_ANIMATION_ASSEMBLER_H
+
+#include "Animators/AnimationAssetId.h"
+#include "ModelAnimationDtos.h"
+#include "AnimationTimeSRT.h"
+
+namespace Enigma::Renderables
+{
+    class AnimationTimeSRTAssembler
+    {
+    public:
+        AnimationTimeSRTAssembler& scaleKey(const AnimationTimeSRT::ScaleKey& key);
+        AnimationTimeSRTAssembler& rotationKey(const AnimationTimeSRT::RotationKey& key);
+        AnimationTimeSRTAssembler& translationKey(const AnimationTimeSRT::TranslateKey& key);
+
+        Engine::GenericDto toGenericDto();
+
+    protected:
+        std::vector<AnimationTimeSRT::ScaleKey> m_scaleKeys;
+        std::vector<AnimationTimeSRT::RotationKey> m_rotationKeys;
+        std::vector<AnimationTimeSRT::TranslateKey> m_translationKeys;
+        AnimationTimeSRTDto m_dto;
+    };
+
+    class ModelAnimationAssembler
+    {
+    public:
+        ModelAnimationAssembler(const Animators::AnimationAssetId& id);
+
+        ModelAnimationAssembler& nodeSRT(const std::string& node_name, const AnimationTimeSRTAssembler& assembler);
+        ModelAnimationAssembler& asAsset(const std::string& name, const std::string& filename, const std::string& path_id);
+
+        Engine::GenericDto toGenericDto();
+
+    protected:
+        std::vector<std::tuple<std::string, AnimationTimeSRTAssembler>> m_nodeSRTs;
+        ModelAnimationAssetDto m_dto;
+    };
+}
+
+#endif // MODEL_ANIMATION_ASSEMBLER_H

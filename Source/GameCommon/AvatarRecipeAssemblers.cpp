97e1df07680d32eaba5fa895f93cd55ca725371c
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2024-09-15 17:23:17 +0800

    [WIP] avatar recipe assemblers

---
+++ Source/GameCommon/AvatarRecipeAssemblers.cpp
@@ -0,0 +1,34 @@
+﻿#include "AvatarRecipeAssemblers.h"
+#include "AvatarRecipes.h"
+
+using namespace Enigma::GameCommon;
+
+static std::string TOKEN_OLD_MATERIAL_ID = "OldMaterialId";
+static std::string TOKEN_NEW_MATERIAL_ID = "NewMaterialId";
+static std::string TOKEN_MESH_ID = "MeshId";
+static std::string TOKEN_TEXTURE_MAPPING_DTO = "TextureMappingDto";
+
+AvatarRecipeAssembler::AvatarRecipeAssembler() : m_factoryDesc(AvatarRecipe::TYPE_RTTI)
+{
+}
+
+Enigma::Engine::GenericDto AvatarRecipeAssembler::assemble() const
+{
+    Enigma::Engine::GenericDto dto;
+    dto.addRtti(m_factoryDesc);
+    return dto;
+}
+
+ReplaceAvatarMaterialAssembler::ReplaceAvatarMaterialAssembler() : AvatarRecipeAssembler()
+{
+    m_factoryDesc = Engine::FactoryDesc(ReplaceAvatarMaterial::TYPE_RTTI);
+}
+
+Enigma::Engine::GenericDto ReplaceAvatarMaterialAssembler::assemble() const
+{
+    Enigma::Engine::GenericDto dto = AvatarRecipeAssembler::assemble();
+    dto.addOrUpdate(TOKEN_OLD_MATERIAL_ID, m_oldMaterialId.name());
+    dto.addOrUpdate(TOKEN_NEW_MATERIAL_ID, m_newMaterialId.name());
+    return dto;
+}
+
---
+++ Source/GameCommon/AvatarRecipeAssemblers.h
@@ -0,0 +1,46 @@
+﻿/*********************************************************************
+ * \file   AvatarRecipeAssemblers.h
+ * \brief
+ *
+ * \author Lancelot 'Robin' Chen
+ * \date   September 2024
+ *********************************************************************/
+#ifndef AVATAR_RECIPE_ASSEMBLERS_H
+#define AVATAR_RECIPE_ASSEMBLERS_H
+
+#include "GameEngine/GenericDto.h"
+#include "GameEngine/EffectMaterialId.h"
+
+namespace Enigma::GameCommon
+{
+    class AvatarRecipeAssembler
+    {
+    public:
+        AvatarRecipeAssembler();
+        virtual ~AvatarRecipeAssembler() = default;
+
+        void factory(const Engine::FactoryDesc& factoryDesc) { m_factoryDesc = factoryDesc; }
+
+        virtual Engine::GenericDto assemble() const;
+
+    protected:
+        Engine::FactoryDesc m_factoryDesc;
+    };
+    class ReplaceAvatarMaterialAssembler : public AvatarRecipeAssembler
+    {
+    public:
+        ReplaceAvatarMaterialAssembler();
+        virtual ~ReplaceAvatarMaterialAssembler() = default;
+
+        void oldMaterialId(const Engine::EffectMaterialId& oldMaterialId) { m_oldMaterialId = oldMaterialId; }
+        void newMaterialId(const Engine::EffectMaterialId& newMaterialId) { m_newMaterialId = newMaterialId; }
+
+        Engine::GenericDto assemble() const override;
+
+    protected:
+        Engine::EffectMaterialId m_oldMaterialId;
+        Engine::EffectMaterialId m_newMaterialId;
+    };
+}
+
+#endif // AVATAR_RECIPE_ASSEMBLERS_H
